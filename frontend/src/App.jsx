029074ab9647d64d0408bb5fb43bde643ed2e528
Suraj-Samgir <surajsamgir2004@gmail.com>
2024-06-16 00:17:19 +0530

    All components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,14 +6,26 @@
 import Navbar from "./components/Navbar";
 import Carousal from "./components/Carousal";
 import Footer from "./components/Footer";
-
+import About from "./components/About";
+import GoatFellow from "./components/GoatFellowForm"
+import ClassCards from "./components/ClassCards";
+import AttendanceTable from "./components/AttendanceTable";
+import AttendanceEvaluation from "./components/AttendanceEvaluation";
+import EvaluationTable from "./components/EvaluationTable";
 function App() {
 
   return (
     <>
       <Navbar></Navbar>
       <Carousal></Carousal>
-      <Footer></Footer>
+      {/* <About></About> */}
+      {/* <EducationFellow></EducationFellow> */}
+      {/* <ClassCards></ClassCards> */}
+      {/* <AttendanceEvaluation></AttendanceEvaluation> */}
+      {/* <AttendanceTable></AttendanceTable> */}
+      {/* <EvaluationTable></EvaluationTable> */}
+      <GoatFellow></GoatFellow>
+      <Footer></Footer> 
     </>
   )
 }
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -14,9 +14,7 @@ const Navbar = () => {
         <li className="nav-item">
           <a className="nav-link active" aria-current="page" href="/">Home</a>
         </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/">About</a>
-        </li>
+        
       </ul>
         <button className="btn btn-danger">Sign-Out</button>
     </div>
