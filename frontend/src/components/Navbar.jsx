185e432304800b161254e5dcb7572948c9fa83e8
Satyam1345 <Satyamkumar1345@gmail.com>
2024-06-15 23:29:22 +0530

    Login , SignUp Done along with Routing

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,29 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Navbar = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate();
+
+  const handleLoginClick = () => {
+    navigate('/login');
+  };
+
+  const handleSignUpClick = () => {
+    navigate('/register');
+  };
+
+  const handleSignOutClick = () => {
+    setIsLoggedIn(false);
+    // Perform additional sign-out logic here if needed
+  };
+
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/">Gram Urja</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/">About</a>
-        </li>
-      </ul>
-        <button className="btn btn-danger">Sign-Out</button>
+        <div className="container-fluid">
+          <NavLink className="navbar-brand" to="/">Gram Urja</NavLink>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink className="nav-link" aria-current="page" to="/">Home</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/">About</NavLink>
+              </li>
+            </ul>
+            <div className="d-flex">
+              {!isLoggedIn ? (
+                <>
+                  <button className="btn btn-success me-2" onClick={handleLoginClick}>Login</button>
+                  <button className="btn btn-primary" onClick={handleSignUpClick}>Sign-Up</button>
+                </>
+              ) : (
+                <button className="btn btn-danger" onClick={handleSignOutClick}>Sign-Out</button>
+              )}
+            </div>
+          </div>
+        </div>
+      </nav>
     </div>
-  </div>
-</nav>
-    </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
